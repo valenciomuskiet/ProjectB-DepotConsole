28d19d2fd85e84230ac99639b3a9fe011d441a3a
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-06-19 10:36:05 +0200

    Update Program.cs
    bezoekers menu update
    - reservering opslaan naar json
    - code controle bij invoeren 
    - weergave als code juist/onjuist is
    - overige kleine aanpassingen

--- DepotConsole/Program.cs
+++ DepotConsole/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace DepotConsole
 {
@@ -23,25 +26,23 @@ namespace DepotConsole
                 }
             }
         }
+
         private static int StartMenuDepot()
         {
             while (true)
             {
-                Console.Clear();
+                LeegPagina();
                 Console.WriteLine("Het Depot Boijmans Van Beuningen\n\n[1] Bezoeker\n[2] Gids");
 
-                int invoergebruiker = Convert.ToInt32(Console.ReadLine());
+                string invoergebruiker = Console.ReadLine();
                 switch (invoergebruiker)
                 {
-                    case 1:
+                    case "1":
                         return 1;
-                    case 2:
+                    case "2":
                         return 2;
-                    case 6:
-                        Console.WriteLine("Closing the application. See you next time!");
-                        return -1;
                     default:
-                        Console.WriteLine("Invalid option. Please only enter the number of the option you would like to pick.\nPress 'Enter' to continue.");
+                        Console.WriteLine("Invoer onjuist, selecteer een van bovenstaande opties a.u.b.");
                         Console.ReadLine();
                         break;
                 }
@@ -53,24 +54,65 @@ namespace DepotConsole
             while (true)
             {
                 LeegPagina();
-                Console.WriteLine("Bezoeker");
-                Console.WriteLine("[0] om terug te gaan naar het start menu ");
+                Console.WriteLine("Bezoeker\n\n[1] om een reservering te maken \n[0] om terug te gaan ");
 
-                int invoerbezoeker = Convert.ToInt32(Console.ReadLine());
+                string invoerbezoeker = Console.ReadLine();
                 switch (invoerbezoeker)
                 {
-                    case 1:
-                        break;
-                    case 0:
+
+                    case "0":
                         return 0;
 
+                    case "1":
+                        LeegPagina();
+                        Rondleidingen();
+
+                        List<Reservering> AlleReserveringen = new List<Reservering>();
+
+                        int num1 = Convert.ToInt32(Console.ReadLine());
+
+                        LeegPagina();
+                        Console.WriteLine("Door u geselecteerd : " + GetTijdvak(num1));
+                        Console.WriteLine("\n\nvoer jouw unieke ticket code in; ");
+
+                        int code = Convert.ToInt32(Console.ReadLine());
+                        int returnvalue = (DelenDoor17(code));
+
+                        var huidigelijst = File.ReadAllText(@"reservering.Json");
+                        AlleReserveringen = JsonConvert.DeserializeObject<List<Reservering>>(huidigelijst);
+
+
+                        if (returnvalue == 0)
+                        {
+                            AlleReserveringen.Add(new Reservering()
+                            {
+                                Code = code,
+                                Tijd = GetTijdvak(num1),
+                            });
+                            string collectionResult = JsonConvert.SerializeObject(AlleReserveringen);
+
+
+                            Console.WriteLine("klik random toets en enter om te saven");
+                            Console.ReadLine();
+
+                            File.WriteAllText(@"reservering.Json", collectionResult);
+                            LeegPagina();
+                            Console.WriteLine("Opgeslagen !, klik een toets en enter om terug te gaan");
+                            Console.ReadLine();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Niet Opgeslagen, klik een toets en enter om terug te gaan !");
+                            Console.ReadLine();
+                        }
+                        break;
                     default:
-                        Console.WriteLine("Invalid option. Please only enter the number of the option you would like to pick.\nPress 'Enter' to continue.");
+                        Console.WriteLine("Invoer onjuist, selecteer een van bovenstaande opties a.u.b.");
                         Console.ReadLine();
                         break;
                 }
             }
-
         }
 
         private static int GidsMenu()
@@ -78,28 +120,28 @@ namespace DepotConsole
             while (true)
             {
                 LeegPagina();
-                Console.WriteLine("gids menu\n[0] keer terug te keren naar het start menu");
-                int invoergids = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Gids\n\n[0] om terug te gaan naar het start menu\n[1] om een overzicht te zien van de reserveringen");
+                string invoergids = Console.ReadLine();
 
                 switch (invoergids)
                 {
-                    case 1: break;
-                    case 0: return 0;
+                    case "1":
+                        break;
+                    case "0": return 0;
 
                     default:
-                        Console.WriteLine("Invalid option. Please only enter the number of the option you would like to pick.\nPress 'Enter' to continue.");
+                        Console.WriteLine("Invoer onjuist, selecteer een van bovenstaande opties a.u.b.");
                         Console.ReadLine();
                         break;
                 }
-
             }
         }
-        
+
         static void LeegPagina()
         {
             Console.Clear();
         }
-        
+
         static int DelenDoor17(int code)
         {
             int answer = code % 17;
@@ -111,7 +153,7 @@ namespace DepotConsole
             else
                 return answer;
         }
-        
+
         static string GetTijdvak(int tijdOptie)
         {
             string Tijdvak;
@@ -144,6 +186,16 @@ namespace DepotConsole
                     break;
             }
             return Tijdvak;
+        }
+
+        static void Rondleidingen()
+        {
+            var dateNu = DateTime.Now;
+            Console.WriteLine("Huidige datum: " + dateNu);
+            Console.WriteLine("Plekken vrij voor vandaag:");
+            Console.WriteLine("Rondleidingen:\n\n[1] 11:00 - 11:20\n[2] 12:00 - 12:20\n[3] 13:00 - 13:20\n[4] 14:00 - 14:20\n[5] 15:00 - 15:20\n[6] 16:00 - 16:20\n[7] 17:00 - 17:20\n");
+            Console.Write("\n\nSelecteer (getal) de Rondleiding naar keuze: ");
+
         }
     }
 }
