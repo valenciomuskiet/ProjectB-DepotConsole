81fe80b3c178d61fa41f405702496abdc7a537df
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-06-19 10:29:45 +0200

    Reservering.cs update
    class voor reservering aangemaakt.

---
+++ DepotConsole/Reservering.cs
@@ -0,0 +1,13 @@
+using System;
+namespace DepotConsole
+{
+	public class Reservering
+	{
+		public int Code { get; set; }
+		public string Tijd { get; set; }
+
+		public Reservering()
+		{
+		}
+	}
+}
